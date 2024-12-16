3aa48e1b1e72fd45a0b5644065a8d8b0b9a1fd7f
Osman Pereira <osmanpdo@gmail.com>
2024-12-16 18:55:04 -0300

    adiciona controladores e modelos para agendamentos, clientes, compras, produtos e serviços

---
+++ controller/CompraController.php
@@ -0,0 +1,21 @@
+<?php
+
+class CompraController {
+
+    public function __construct() {}
+
+    public function index() {}
+
+    public function show(int $id) {}
+
+    public function create() {}
+
+    public function store() {}
+
+    public function edit(int $id) {}
+
+    public function update(int $id) {}
+
+    public function delete(int $id) {}
+}
+
