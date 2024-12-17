8c6e1f70469f15bb620794a73848d0efc59f6ffc
Osman Pereira <osmanpdo@gmail.com>
2024-12-17 07:54:53 -0300

    adiciona lógica de controle dinâmico e testes para agendamentos, clientes, produtos e serviços

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 
 include_once __DIR__ . '/controller/AgendamentoController.php';
+include_once __DIR__ . '/controller/ClienteController.php';
+include_once __DIR__ . '/controller/CompraController.php';
+include_once __DIR__ . '/controller/ProdutoController.php';
+include_once __DIR__ . '/controller/ServicoController.php';
 
-$controller = new AgendamentoController();
+$classe = $_GET['classe'];
+$metodo = $_GET['metodo'];
+$id = $_GET['id'];
 
-$controller->edit(1);
+if (!isset($classe) || !isset($metodo)) {
+    header('Location: view/home/homepage.php');
+    exit();
+}
+
+$classe .= 'Controller';
+
+$controller = new $classe();
+
+if (!isset($id)) {
+    $controller->$metodo();
+} else {
+    $controller->$metodo($id);
+}
